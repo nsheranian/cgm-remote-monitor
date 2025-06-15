531b28fd818182a5064c1096075ffac0116e1c66
nsheranian <35147094+nsheranian@users.noreply.github.com>
2025-06-15 13:07:54 -0600

    Add CrowdVue step1 skeleton

---
+++ crowdvue/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '1rem' }}>
+      <h1>CrowdVue MVP</h1>
+      <p>Step 1: Project Setup</p>
+    </div>
+  );
+}
+
+export default App;
